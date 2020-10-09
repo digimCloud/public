75adad748ff9a3aadb5c1aa3578b0b3df31e4552
digimCloud <cloud@digitalmedia.cz>
2020-10-09 11:12:14 +0200

    Update exportSelected-to-new-files.py

--- C4D/scripts/exportSelected-to-new-files.py
+++ C4D/scripts/exportSelected-to-new-files.py
@@ -13,31 +13,25 @@ def main():
     
     docOrig = c4d.documents.GetActiveDocument()
     selected = docOrig.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN)
-    
     savePath = docOrig.GetDocumentPath()
     
     if selected == None:
+        print("Nebyl vybrán žádný objekt. Vyberte alespoň jeden objekt a zkuste to znovu.")
         return
-
 
     objNames = []
     objDuplicateNames = []
     
     for obj in selected:
-        
         fileBaseName = obj.GetName()
-        
         if fileBaseName in objNames:
             print("POZOR! Objekt s názvem " + fileBaseName + " již byl exportován.")
             objDuplicateNames.append(fileBaseName)
         else:
             objNames.append(fileBaseName)
-        
             newDoc = c4d.documents.BaseDocument() 
             newDoc.SetDocumentName(fileBaseName)
-            
             newDoc.InsertObject(obj.GetClone())
-            
             saveFile = savePath + "/export/" + fileBaseName + ".c4d"
             c4d.documents.SaveDocument(newDoc, saveFile, c4d.SAVEDOCUMENTFLAGS_0, c4d.FORMAT_C4DEXPORT)
             c4d.documents.KillDocument(newDoc)
@@ -46,8 +40,6 @@ def main():
     print("*******************")
     print("vyexportováno " + str(len(objNames)) + " souborů")
     print("nalezeno " + str(len(objDuplicateNames)) + " duplicitních názvů ")
-        
-        
     
 # Execute main()
 if __name__=='__main__':
